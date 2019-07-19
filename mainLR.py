ba9b3f61cd098e750286cdd08bc07605e74a42be
kedrboje <fmrubchenkov@edu.hse.ru>
2019-07-19 16:20:51 +0300

    added get_uid_model func and show_plt

--- mainLR.py
+++ mainLR.py
@@ -39,8 +39,30 @@ class Tst_class():
     def preview_data(self):
         print(self.df.tail(25))
 
+    def get_uid_model(self, uid: int):
+        main_df = self.df[self.df['Uid code'] == uid].copy()
+        x = main_df[['Sale', 'ND']].values
+        y = main_df['PoS'].values
+        X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+        model = LinearRegression().fit(X_train, Y_train)
+        coeff_df = pd.DataFrame(model.coef_, main_df[['Sale', 'ND']].columns, columns=['Coefficient'])
+        y_pred = model.predict(X_test)
+        df_pred = pd.DataFrame({'Actual': Y_test, 'Predicted': y_pred})
+        self.show_plt(df_pred)
+        return model, coeff_df, df_pred
+
+    def show_plt(self, df_pred):
+        main_df = df_pred
+        main_df.plot(kind='bar', figsize=(15, 10))
+        plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+        plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+        plt.show()
+
 
 dirpath = 'test_data1.xlsx'
 lr = Tst_class()
 lr.preprocessing_data(dirpath)
 lr.preview_data()
+for i in range(71):
+    model, coef, df_pred = lr.get_uid_model(i)
+    lr.show_plt(df_pred)
